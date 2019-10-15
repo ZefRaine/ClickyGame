54ec3219b9806f728a9f5a42af7f6b20c11ff533
Zeph Tambornino <zeftambornino@gmail.com>
2019-10-15 13:30:18 -0500

    Init Commit

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,12 +4,13 @@ import "./style.css";
 function Card(props) {
   return (
     <div className="card">
-      <div className="img-container">
+      <div className="img-container" onClick={props.shuffle}>
         <img alt={props.name} src={props.image} />
       </div>
     </div>
   );
 }
+
 //function random() {math.floor}
 export default Card;
 //randomizer in own function cb in main function
